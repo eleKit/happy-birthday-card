508318dc27537cab83c642b31ee0150942ec72db
Eleonora Chitti <eleonora.chitti@unimi.it>
2024-06-14 12:57:16 +0200

    upgraded to Unity 2022

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -62,5 +63,10 @@ public class GameManager : MonoBehaviour
         music.PlayMusic("Applause");
    
         
+    }
+
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene("HappyBirthdayScene");
     }
 }
